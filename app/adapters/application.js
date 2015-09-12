339415b3deb4aabd8bba961fab996f2d40e19277
Damien Garaud <damien.garaud@gmail.com>
2015-09-12 15:57:54 +0200

    draft REST adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,24 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: "http://localhost:5000"
-  namespace: 'api'
+  namespace: 'api',
 });
+
+// App.Adapter.map('App.FeatureCollection', {
+//   features: { embedded: 'always' }
+// });
+
+// App.Adapter.map('App.Feature', {
+//   geometry: { embedded: 'always' },
+//   properties: { embedded: 'always' }
+// });
+
+// a point
+// DS.RESTAdapter.registerTransform('point', {
+//   serialize: function(value) {
+//     return [value.get('x'), value.get('y')];
+//   },
+//   deserialize: function(value) {
+//     return Ember.create({ x: value[1], y: value[0] });
+//   }
+// });
